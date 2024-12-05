0b88cdc61cbeba156691d1b06d3d440cf9e37f27
ypelletier <yan.pelletier@gmail.com>
2024-12-04 22:44:08 -0500

    ...

--- BqprB/keymap.c
+++ BqprB/keymap.c
@@ -478,3 +478,11 @@ tap_dance_action_t tap_dance_actions[] = {
 
 
 
+
+// Custom QMK here
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+
+// This globally defines all key overrides to be used
+const key_override_t *key_overrides[] = {
+	&delete_key_override
+};
