cc8862b651ec64dfce7e199038e9635547cb935e
ypelletier <yan.pelletier@gmail.com>
2024-12-04 22:13:34 -0500

    Delete when pressing Shift + Backspace

--- BqprB/keymap.c
+++ BqprB/keymap.c
@@ -475,3 +475,13 @@ tap_dance_action_t tap_dance_actions[] = {
         [DANCE_5] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_5, dance_5_finished, dance_5_reset),
         [DANCE_6] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_6, dance_6_finished, dance_6_reset),
 };
+
+
+
+
+// Custom QMK here
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+
+const key_override_t *key_overrides[] = {
+	&delete_key_override
+};
